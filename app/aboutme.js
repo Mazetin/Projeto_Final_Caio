fc3e57d34a79ba77e399221cb8ad473d1ce0feff
Brenokubi0 <breno.furlan@etec.sp.gov.br>
2025-11-24 09:03:58 -0300

    24/04

--- app/aboutme.js
+++ app/aboutme.js
@@ -1,19 +1,36 @@
 import { StatusBar } from "react-native-web";
-import { Image, StyleSheet, Text, View, Platform } from "react-native";
+import { Image, StyleSheet, Text, View, Platform, ImageBackground, } from "react-native";
 import foto from "../assets/img.jpg";
+import { SafeAreaView } from "react-native-safe-area-context";
+import background from "../assets/painel.jpg";
 
 export default function Page() {
   return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Sobre mim</Text>
-        <View>
-          <Image source={foto} style={styles.foto} />
+    <SafeAreaView style={{ flex: 1 }}>
+      <ImageBackground source={background} style={styles.background} resize="center">
+        <View style={styles.container}>
+          <Text style={styles.title}>Sobre mim</Text>
+          <View style={styles.main}>
+            <View>
+              <Image source={foto} style={styles.foto} />
         </View>
-        <View></View>
-        <Text style={styles.subtitle}>Detalhes estudantis</Text>
+        <View>
+          <Text style={styles.subtitle}>
+          <Text style={{fontWeight: "bold"}}>Nome: </Text>Caio Mazeti</Text>
+          <Text style={styles.subtitle}>
+            <Text style={{fontWeight: "bold"}}>Idade: </Text>16 anos</Text>
+            <Text style={styles.subtitle}>
+            <Text style={{fontWeight: "bold"}}>RM: </Text>08299</Text>
+          <Text style={styles.subtitle}>
+            <Text style={{fontWeight: "bold"}}>Curso: </Text>Técnico em Informática</Text>
+          <Text style={styles.subtitle}>
+            <Text style={{fontWeight: "bold"}}>Escola: </Text>ETEC Milton Gazzetti</Text>
+        </View>
+        <StatusBar style="auto" />
       </View>
     </View>
+    </ImageBackground>
+    </SafeAreaView>
   );
 }
 
@@ -25,15 +42,19 @@ const styles = StyleSheet.create({
     padding: 24,
   },
   main: {
-    flex: 1,
+    flexDirection: Platform.OS === "web" ? "row" : "column",
+    gap: 20,
     justifyContent: "center",
+    alignItems: "center",
     maxWidth: 960,
     marginHorizontal: "auto",
   },
   title: {
     fontSize: 42,
-    fontWeight: "bold",
+    fontSize: Platform.OS === "web" ? 48 : 36,
+    fontWeight: Platform.OS === "web" ? "bold" : null,
     marginBottom: 20,
+    textTransform: "uppercase",
   },
   subtitle: {
     fontSize: 36,
@@ -44,5 +65,10 @@ const styles = StyleSheet.create({
     height: 200,
     borderRadius: 15,
     marginBottom: 20,
+  },
+  background:{
+    height: "100%",
+    width: "100%",
+    flex: 1,
   }
 });
